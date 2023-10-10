ed67513e727c10540806cf6715a88c1a2bade2c5
Amirhossein Moznebi <59997964+amirimoo@users.noreply.github.com>
2023-10-10 15:43:41 +0330

    Snake Info Function Added

--- snake.py
+++ snake.py
@@ -16,17 +16,38 @@ else :
     print("Game Succesfully intialized")
 
 
+
 pygame.display.set_caption("Snake Game")
 
 Game_window = pygame.display.set_mode(frame_size_x,frame_size_y)
 
-#
 
+#start colors set
 black = pygame.Color(0,0,0)
 white = pygame.Color(255,255,255)
 red   = pygame.Color(255,0,0)
 green = pygame.Color(0,255,0)
 blue  = pygame.Color(0,0,255)
+#endcolors set
+
+fps_controller = pygame.time.Clock()
+
+#one snake square size
+square_size = 20 
 
 
+#Start snake info Function
+def init_vars():
+   global head_pos , snake_body , food_pos, food_spawn , score , direction 
+   direction  = "RIGHT"
+   head_pos   = [120,60]
+   snake_body = [[120 , 60]]
+   food_pos   = [random.randrange(1,(frame_size_x // square_size)) * square_size, 
+                 random.randrange(1,(frame_size_y // square_size)) * square_size]
+   food_spawn = True
+   score      = 0
 
+
+init_vars()
+
+#End Snake info Function 
